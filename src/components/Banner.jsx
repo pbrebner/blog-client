98c6141599d85e054b2570cbe753c488f627b6a4
Patrick Brebner <patrick.brebner@gmail.com>
2023-12-07 15:06:39 -0800

    Create banner component

---
+++ src/components/Banner.jsx
@@ -0,0 +1,14 @@
+function Banner() {
+    return (
+        <div className="banner">
+            <h1 className="bannerTitle">Explore new perspectives</h1>
+            <p className="bannerPara">
+                Read and share ideas from independent voices, world-class
+                publications, and experts from arpound the globe. Everyone's
+                welcome
+            </p>
+        </div>
+    );
+}
+
+export default Banner;
