a796c7943d3937bf77582bb3f5e4305217be8a07
Patrick Brebner <patrick.brebner@gmail.com>
2023-12-07 15:14:35 -0800

    Set up App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
+import { Outlet } from "react-router-dom";
+
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 import "./App.css";
 
 function App() {
+    let [loggedIn, setLoggedIn] = useState(false);
+
     return (
         <>
-            <div>Main App</div>
+            <Header />
+            <h1>This is the main App Page</h1>
+            <Outlet />
+            <Footer />
         </>
     );
 }
