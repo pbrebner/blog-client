57d360f8aa0f12639c52b9efa6e7e798d2f0fe78
Patrick Brebner <patrick.brebner@gmail.com>
2023-12-07 15:13:24 -0800

    Add browser router and routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from "./App.jsx";
+import "./index.css";
+
+import ErrorPage from "./pages/ErrorPage.jsx";
+import Home from "./pages/Home.jsx";
+import Signup from "./pages/Signup.jsx";
+import Login from "./pages/Login.jsx";
+import Post from "./pages/Post.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            { index: true, element: <Home />, errorElement: <ErrorPage /> },
+            {
+                path: "account",
+                errorElement: <ErrorPage />,
+                children: [
+                    { path: "signup", element: <Signup /> },
+                    { path: "login", element: <Login /> },
+                    { path: ":id", element: <Profile /> },
+                ],
+            },
+            {
+                path: "posts/:id",
+                element: <Post />,
+                errorElement: <ErrorPage />,
+            },
+        ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
