4c72938115ace582adcbcb614b8445742ba14813
Patrick Brebner <patrick.brebner@gmail.com>
2023-12-12 18:24:31 -0800

    Add logic to fetch profile data

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,60 @@
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 function Profile() {
+    const [user, setUser] = useState(null);
+    const [error, setError] = useState(null);
+
+    const { userId } = useParams();
+
+    // Fetch the post and display contents
+    useEffect(() => {
+        async function getUser() {
+            try {
+                const response = await fetch(
+                    `https://blog-api-test.fly.dev/api/users/${userId}`,
+                    {
+                        headers: {
+                            "Content-Type": "application/json",
+                            authorization: `Bearer ${localStorage.getItem(
+                                "token"
+                            )}`,
+                        },
+                    }
+                );
+
+                if (!response.ok) {
+                    throw new Error(
+                        `This is an HTTP error: The status is ${response.status}`
+                    );
+                }
+
+                const data = await response.json();
+                console.log(data);
+                setUser(data);
+                setError(null);
+            } catch (err) {
+                setError(err.message);
+                setUser(null);
+            }
+        }
+        getUser();
+    }, []);
+
     return (
-        <div className="main">
-            <div>Profile Page</div>
+        <div className="main profilePage">
+            {error && (
+                <div className="errorContainer">
+                    There was problem fetching the data from the server. Please
+                    try again later
+                </div>
+            )}
+            {user && (
+                <div className="userContainer">
+                    <h3>{user.name}</h3>
+                    <p>{user.username}</p>
+                </div>
+            )}
         </div>
     );
 }
