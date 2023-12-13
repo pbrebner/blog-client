a2e1dc51fdf6808b7b292fdc013e57e7741e489d
Patrick Brebner <patrick.brebner@gmail.com>
2023-12-12 18:22:19 -0800

    Add logic to logout and display diffenent nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,47 @@
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./styles/Header.css";
 
-function Header() {
-    return (
-        <header className="header">
-            <Link to="/" className="headerTitle">
-                Blog
-            </Link>
-            <Link to="/account/login" className="navLink">
-                Log in
-            </Link>
-        </header>
-    );
+function Header({ loggedIn, setLoggedIn }) {
+    const navigate = useNavigate();
+
+    function handleLogout() {
+        localStorage.clear();
+        setLoggedIn(false);
+        navigate("/");
+    }
+
+    if (loggedIn) {
+        return (
+            <header className="header">
+                <Link to="/" className="headerTitle">
+                    Blog
+                </Link>
+                <div className="navLinks">
+                    <Link
+                        to={`/account/${localStorage.getItem("userId")}`}
+                        className="navLink"
+                    >
+                        Account
+                    </Link>
+                    <Link onClick={handleLogout} className="navLink">
+                        Log out
+                    </Link>
+                </div>
+            </header>
+        );
+    } else {
+        return (
+            <header className="header">
+                <Link to="/" className="headerTitle">
+                    Blog
+                </Link>
+                <Link to="/account/login" className="navLink">
+                    Log in
+                </Link>
+            </header>
+        );
+    }
 }
 
 export default Header;
