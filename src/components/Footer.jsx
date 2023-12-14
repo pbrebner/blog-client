e787a53ddc9307fc17cbdedfd909b7b3419f99bc
Patrick Brebner <patrick.brebner@gmail.com>
2023-12-14 14:34:05 -0800

    Update Footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,30 +4,33 @@ import "./styles/Footer.css";
 function Footer() {
     return (
         <footer className="footer">
-            <div className="footerLeftCol span2Row">
-                <h3>Get in the know</h3>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Numquam quo odio nostrum mollitia ratione beatae laboriosam
-                    culpa nesciunt repellat deleniti deserunt dolorem, eligendi
-                    voluptates id, vero amet molestiae eveniet quia.
-                </p>
-            </div>
-            <div className="footerRightCol">
-                <div>
-                    <h4>About</h4>
-                    <a href="">About Us</a>
-                    <a href="">Our Vision</a>
+            <div className="footerContainer">
+                <div className="footerLeftCol span2Row">
+                    <h3>Get in the know</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Numquam quo odio nostrum mollitia ratione beatae
+                        laboriosam culpa nesciunt repellat deleniti deserunt
+                        dolorem, eligendi voluptates id, vero amet molestiae
+                        eveniet quia.
+                    </p>
                 </div>
-                <div>
-                    <h4>Contact Us</h4>
-                    <a href="">Advertise</a>
-                    <a href="">Email</a>
+                <div className="footerRightCol">
+                    <div>
+                        <h4>About</h4>
+                        <a href="">About Us</a>
+                        <a href="">Our Vision</a>
+                    </div>
+                    <div>
+                        <h4>Contact Us</h4>
+                        <a href="">Advertise</a>
+                        <a href="">Email</a>
+                    </div>
                 </div>
-            </div>
-            <div className="acknowledgements">
-                <hr />
-                <p>Developed by Patrick Brebner</p>
+                <div className="acknowledgements">
+                    <hr />
+                    <p>Developed by Patrick Brebner</p>
+                </div>
             </div>
         </footer>
     );
