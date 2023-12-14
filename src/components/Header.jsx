3c26b13431731d0bcd60c6aedab343fa447445b1
Patrick Brebner <patrick.brebner@gmail.com>
2023-12-14 14:35:48 -0800

    Update styling and add menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,44 +1,71 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import "./styles/Header.css";
 
 function Header({ loggedIn, setLoggedIn }) {
+    const [isMenuOpen, setMenuOpen] = useState(false);
     const navigate = useNavigate();
 
     function handleLogout() {
         localStorage.clear();
         setLoggedIn(false);
+        setMenuOpen(false);
         navigate("/");
+    }
+
+    function handleMenuClick() {
+        setMenuOpen(false);
+    }
+
+    function toggleMenuOpen() {
+        isMenuOpen === false ? setMenuOpen(true) : setMenuOpen(false);
     }
 
     if (loggedIn) {
         return (
-            <header className="header">
-                <Link to="/" className="headerTitle">
-                    Blog
-                </Link>
-                <div className="navLinks">
+            <>
+                <header className="header">
+                    <Link to="/" className="headerTitle">
+                        Little Things Everyday
+                    </Link>
+                    <div className="nav">
+                        <Link to="/posts/createpost" className="navLink write">
+                            Write
+                        </Link>
+                        <button onClick={toggleMenuOpen} className="menuBtn">
+                            Menu
+                        </button>
+                    </div>
+                </header>
+                <div className={`menuTab ${isMenuOpen ? "display" : ""}`}>
                     <Link
                         to={`/account/${localStorage.getItem("userId")}`}
-                        className="navLink"
+                        onClick={handleMenuClick}
+                        className="menuLink"
                     >
                         Account
                     </Link>
-                    <Link onClick={handleLogout} className="navLink">
-                        Log out
+                    <Link onClick={handleLogout} className="menuLink">
+                        Sign out
                     </Link>
                 </div>
-            </header>
+            </>
         );
     } else {
         return (
             <header className="header">
                 <Link to="/" className="headerTitle">
-                    Blog
+                    Little Things Everyday
                 </Link>
-                <Link to="/account/login" className="navLink">
-                    Log in
-                </Link>
+                <div className="nav">
+                    <Link to="/account/login" className="navLink signin">
+                        Sign in
+                    </Link>
+                    <Link to="/account/signup" className="navLink signup">
+                        Sign up
+                    </Link>
+                </div>
             </header>
         );
     }
