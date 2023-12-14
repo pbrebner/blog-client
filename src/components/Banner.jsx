23c8dc95d1408f81b4dcef5be2474188ede56458
Patrick Brebner <patrick.brebner@gmail.com>
2023-12-14 14:35:10 -0800

    Add styling to Banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,12 +1,16 @@
+import "./styles/Banner.css";
+
 function Banner() {
     return (
         <div className="banner">
-            <h1 className="bannerTitle">Explore new perspectives</h1>
-            <p className="bannerPara">
-                Read and share ideas from independent voices, world-class
-                publications, and experts from arpound the globe. Everyone's
-                welcome.
-            </p>
+            <div className="bannerContent">
+                <h1 className="bannerTitle">Explore new perspectives</h1>
+                <p className="bannerPara">
+                    Read and share ideas from independent voices, world-class
+                    publications, and experts from arpound the globe. Everyone's
+                    welcome.
+                </p>
+            </div>
         </div>
     );
 }
