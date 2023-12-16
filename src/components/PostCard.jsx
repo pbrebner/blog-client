a43d3b8a33aadbad38123ec13bfcc6544c8f5090
Patrick Brebner <patrick.brebner@gmail.com>
2023-12-16 15:14:25 -0800

    Add luxon and create script to format dates

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 import "./styles/PostCard.css";
 
+import { formatDate } from "../utils/dates.js";
+
 function PostCard({ post }) {
     return (
         <Link to={`/posts/${post._id}`} className="postCard">
             <div className="postCardHeader">
                 {post.user.name && <p>{post.user.name}</p>}
-                <p>{post.timeStamp}</p>
+                <p>{formatDate(post.timeStamp)}</p>
             </div>
             <div className="postCardContent">
                 <h4 className="postCardTitle">{post.title}</h4>
