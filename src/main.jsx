92fb86ada7be5ea7210479567badd3190d9fd68d
Patrick Brebner <patrick.brebner@gmail.com>
2024-01-24 14:53:50 -0800

    Update app path

--- src/main.jsx
+++ src/main.jsx
@@ -16,7 +16,7 @@ import Post from "./pages/Post.jsx";
 
 const router = createBrowserRouter([
     {
-        path: "/",
+        path: "/blog-client",
         element: <App />,
         errorElement: <ErrorPage />,
         children: [
