88cd52b00956536fb712001d51932379de5eed35
Patrick Brebner <patrick.brebner@gmail.com>
2024-01-28 17:02:07 -0800

    Add page loader

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 
 import Banner from "../components/Banner.jsx";
 import PostCard from "../components/PostCard.jsx";
+import PostCardLoader from "../components/PostCardLoader.jsx";
 
 import "./styles/Home.css";
 
@@ -9,8 +10,12 @@ function Home() {
     const [posts, setPosts] = useState(null);
     const [error, setError] = useState(null);
 
+    const [pageLoading, setPageLoading] = useState(false);
+
     // Fetch all posts and display on "home page"
     useEffect(() => {
+        setPageLoading(true);
+
         async function getPosts() {
             try {
                 const response = await fetch(
@@ -25,15 +30,25 @@ function Home() {
 
                 const data = await response.json();
                 console.log(data);
+
+                setPageLoading(false);
                 setPosts(data);
                 setError(null);
             } catch (err) {
                 setError(err.message);
                 setPosts(null);
+                setPageLoading(false);
             }
         }
         getPosts();
     }, []);
+
+    const pageLoadingElements = [];
+    for (let i = 0; i < 6; i++) {
+        pageLoadingElements.push(
+            <PostCardLoader key={i} pageLoading={pageLoading} />
+        );
+    }
 
     return (
         <div className="main homePage">
@@ -45,19 +60,22 @@ function Home() {
                 </div>
             )}
             <div className="homeMain">
-                {posts && (
-                    <div className="postsContainer">
-                        {posts.map((post) => (
-                            <div
-                                key={post._id}
-                                className="postCardOuterContainer"
-                            >
-                                <PostCard post={post} />
-                                <div className="hl"></div>
-                            </div>
-                        ))}
-                    </div>
-                )}
+                <div className="postsOuterContainer">
+                    {pageLoadingElements}
+                    {posts && (
+                        <div className="postsContainer">
+                            {posts.map((post) => (
+                                <div
+                                    key={post._id}
+                                    className="postCardOuterContainer"
+                                >
+                                    <PostCard post={post} />
+                                    <div className="hl"></div>
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     );
