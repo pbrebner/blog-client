c30cc34f50474eeb85856d1be74eab7c1a4ac61b
Patrick Brebner <patrick.brebner@gmail.com>
2024-01-29 20:18:00 -0800

    Add error handling

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,12 +9,14 @@ import {
 import Button from "../components/Button";
 import "./styles/FormPages.css";
 
-// TODO: Add action to form (url of API)
-
 function Login() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+
     const [showLoader, setShowLoader] = useState(false);
+
+    const [formError, setFormError] = useState("");
+    const [error, setError] = useState("");
 
     const [loggedIn, setLoggedIn] = useOutletContext();
     const navigate = useNavigate();
@@ -24,42 +26,62 @@ function Login() {
         e.preventDefault();
         setShowLoader(true);
 
+        setFormError("");
+        setError("");
+
         const formData = JSON.stringify({
             username: username,
             password: password,
         });
 
         // Need to add a try/catch to handle errors
-        const response = await fetch(
-            "https://blog-api-test.fly.dev/api/login",
-            {
-                method: "post",
-                body: formData,
-                headers: { "content-Type": "application/json" },
-            }
-        );
+        try {
+            const response = await fetch(
+                "https://blog-api-test.fly.dev/api/login",
+                {
+                    method: "post",
+                    body: formData,
+                    headers: { "content-Type": "application/json" },
+                }
+            );
 
-        console.log(response);
-        const data = await response.json();
-        console.log(data);
+            console.log(response);
+            const result = await response.json();
+            console.log(result);
 
-        if (data.token) {
-            // Save token to local storage and setLoggedInd
-            localStorage.setItem("token", data.token);
-            localStorage.setItem("userAuth", true);
-            localStorage.setItem("name", data.body.name);
-            localStorage.setItem("userId", data.body._id);
-            setLoggedIn(true);
             setShowLoader(false);
-            navigate("/blog-client");
+
+            if (response.status == 400) {
+                setFormError(result.errors);
+            } else if (!response.ok) {
+                throw new Error(
+                    `This is an HTTP error: The status is ${response.status}`
+                );
+            } else if (result.token) {
+                // Save token to local storage and setLoggedInd
+                localStorage.setItem("token", result.token);
+                localStorage.setItem("userAuth", true);
+                localStorage.setItem("name", result.body.name);
+                localStorage.setItem("userId", result.body._id);
+
+                setLoggedIn(true);
+                navigate("/blog-client");
+            }
+        } catch (err) {
+            setError(err.message);
         }
     }
 
     return (
         <div className="main formPage">
+            {error && (
+                <div className="errorContainer">
+                    There was problem handling your request. Please try again
+                    later.
+                </div>
+            )}
             <h2>Login</h2>
             {location.state && <div>{location.state.message}</div>}
-
             <form className="pageForm">
                 <div className="formElement">
                     <input
@@ -91,6 +113,13 @@ function Login() {
                         disabled={showLoader}
                     />
                 </div>
+                {formError && (
+                    <div className="formErrorContainer">
+                        <ul className="formErrorList">
+                            <li className="formError">{formError}</li>
+                        </ul>
+                    </div>
+                )}
             </form>
             <p>
                 Don't have an account yet?{" "}
