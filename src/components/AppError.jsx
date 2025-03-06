6bc611e17e8cc1eeb3c7c09f239d9355213b7b30
Patrick Brebner <patrick.brebner@gmail.com>
2025-03-06 12:31:32 -0800

    Add appError

---
+++ src/components/AppError.jsx
@@ -0,0 +1,12 @@
+function AppError({ error }) {
+    return (
+        <div className="appError">
+            <div>
+                There was a problem connecting to the server. Please try again
+                later.
+            </div>
+        </div>
+    );
+}
+
+export default AppError;
