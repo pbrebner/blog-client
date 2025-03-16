70f796ea7259cb29d4b887ed175eebc05d4bd6f8
Patrick Brebner <patrick.brebner@gmail.com>
2025-03-16 13:40:06 -0700

    Initial commit

---
+++ src/components/topPosts.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+
+import "./styles/topPosts.css";
+
+function topPosts({ topPosts }) {
+    return <div className="topPostsContainer"></div>;
+}
+
+export default topPosts;
