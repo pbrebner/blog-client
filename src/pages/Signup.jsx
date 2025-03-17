c0d75b6bf82b2e6a8d0b8971d19fa6f870c99766
Patrick Brebner <patrick.brebner@gmail.com>
2025-03-17 14:48:11 -0700

    Fix useContext import error

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useOutletContext } from "react-router-dom";
 
 import Button from "../components/Button";
 import "./styles/FormPages.css";
